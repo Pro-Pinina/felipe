e6a08d2f0e2f685b4f0d20824cf55afcacaaeebd
Fazt <contact@bweb.com>
2019-07-12 16:10:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route} from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { EscritorioComponent } from './escritorio/escritorio.component';
+
+const routes: Route[]= [
+  {path: '',component: InicioComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'escritorio', component:EscritorioComponent}
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    EscritorioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
